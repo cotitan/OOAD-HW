363f04cee6df919c30c4b47358666e137cc9ab11
tiankk <cotitan@outlook.com>
2016-05-29 22:35:09 +0800

    encode and decode for picture

--- testServer/src/main/java/TestServer/Test.java
+++ testServer/src/main/java/TestServer/Test.java
@@ -4,6 +4,12 @@ import src.main.java.TestServer.datamodel.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import com.google.gson.Gson;
+import sun.misc.BASE64Encoder;
+import sun.misc.BASE64Decoder;
+import java.io.FileOutputStream;  
+import java.io.IOException;  
+import java.io.InputStream;  
+import java.io.OutputStream;
 
 public class Test {
     public void testLogin() {
@@ -55,8 +61,36 @@ public class Test {
 	    } catch (Exception e) {
 	        System.out.println("exception deserialize");
 	    }
-	    for (int i = 0; i < list.size(); i++)
-	        System.out.println(list.get(i).getName()); // + "," + list.get(i).getIntro());
+	    for (int i = 0; i < list.size(); i++) {
+	        System.out.println(list.get(i).getName() + " , " + list.get(i).getIntro());
+	        if (deserialize(list.get(i).getAvatar()))
+	            System.out.println("succeed");
+	    }
+    }
+
+    public boolean deserialize(String imgStr) {
+        //对字节数组字符串进行Base64解码并生成图片  
+        if (imgStr == null) //图像数据为空  
+            return false;  
+        BASE64Decoder decoder = new BASE64Decoder();  
+        try {  
+            //Base64解码  
+            byte[] b = decoder.decodeBuffer(imgStr);  
+            for(int i=0;i<b.length;++i) {  
+                if(b[i]<0) {//调整异常数据  
+                    b[i]+=256;  
+                }  
+            }  
+            //生成jpeg图片  
+            String imgFilePath = "pic2.png";//新生成的图片  
+            OutputStream out = new FileOutputStream(imgFilePath);      
+            out.write(b);  
+            out.flush();  
+            out.close();  
+            return true;  
+        } catch (Exception e) {  
+            return false;  
+        }
     }
 
 }
