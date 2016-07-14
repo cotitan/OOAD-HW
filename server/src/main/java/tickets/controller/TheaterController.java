fc90ff5a57d220ecb32b7bda541cc000f46e3e77
bigbigpaohui <1274986251@qq.com>
2016-07-14 19:02:55 +0800

    Theater and Theatercontroller change

--- server/src/main/java/tickets/controller/TheaterController.java
+++ server/src/main/java/tickets/controller/TheaterController.java
@@ -2,9 +2,14 @@ package tickets.controller;
 
 import tickets.DBMSOperation;
 import tickets.datamodel.*;
-import java.sql.ResultSet;
+import sun.misc.BASE64Encoder;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.*;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.PreparedStatement;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,29 +21,34 @@ public class TheaterController {
 	
 	private static Connection dbmsConn = DBMSOperation.getDBConnection();
 	private PreparedStatement sql;
-	private ResultSet res;
 
-    //private String pic;
+
 
 
 	@RequestMapping(value="/nearby", method = RequestMethod.GET, consumes="application/json")
 	public @ResponseBody TheaterList returnTheaterList() {
-	    System.out.println(" { return theater list... }");
-	    /*String theater1 = "广州科学中心巨幕影院";
-	    String theater2 = "金逸珠江国际影城大学城店";
-		String location1 = "广州市番禺区科普路原大学城西";
-		String location2 = "GOGO新天地二期2层";*/
-		String theater1 = "gz";
-	    String theater2 = "ku";
-		String location1 = "dxc";
-		String location2 = "GOGO";
-		String price1 = "20.8";
-		String price2 = "28.8";
-	    TheaterList theaterList = new TheaterList();
-        theaterList.addTheater(new Theater(theater1, location1, price1));
-        theaterList.addTheater(new Theater(theater2, location2, price2));
-		return theaterList;
-	}
+        System.out.println(" { return theater list... }");
+        TheaterList theaterList = new TheaterList();
+        try {
+            String query = "select * from theater ";
+            sql = dbmsConn.prepareStatement(query);
+            ResultSet res = sql.executeQuery();
+            while (res.next()) {
+                Theater t = new Theater(
+                        res.getInt("theaterID"),
+                        res.getString("theaterName"),
+                        res.getString("onShowList"),
+						res.getString("address"),
+						res.getDouble("distance"),
+						res.getDouble("lowestPrice"));
+                theaterList.addTheater(t);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+   
+        return theaterList;
+    }
 
 }
 
--- server/src/main/java/tickets/datamodel/Theater.java
+++ server/src/main/java/tickets/datamodel/Theater.java
@@ -1,41 +1,53 @@
 package tickets.datamodel;
 
 public class Theater {
-    private String name;
-    private String location;
-    private String price;
+	private int theaterID;
+    private String theaterName;
+	private String onShowList;
+    private String address;
+	private double distance;
+	private double lowestPrice
     // distance ???
 	
     public Theater() {}
     
-    public Theater(String name, String location, String price) {
-        this.name = name;
-        this.location = location;
-        this.price = price;
+    public Theater(String name, String address, String price) {
+        this.theaterName = name;
+        this.address = address;
+        this.lowesetPrice = price;
+    }
+	
+	public Theater(int ID, String name, String address, String onShowList, String address, double distance, double price) {
+		this.theaterID = ID;
+        this.theaterName = name;
+		this.onShowList = onShowList;
+        this.address = address;
+		this.distance = distance;
+        this.lowesetPrice = price;
     }
     
     public String getName() {
-        return name;
+        return theaterName;
     }
     
-    public String getLocation() {
-        return location;
+    public String getAddress() {
+        return address;
     }
     
-    public String getPrice() {
-        return price;
+    public double getPrice() {
+        return lowestPrice;
     }
     
     public void setName(String name) {
-        this.name = name;
+        this.theaterName = name;
     }
     
-    public void setLocation(String location) {
-        this.location = location;
+    public void setaddress(String address) {
+        this.address = address;
     }
     
     public void setPrice(String price) {
-        this.price = price;
+        this.lowesetPrice = price;
     }
 }
 
